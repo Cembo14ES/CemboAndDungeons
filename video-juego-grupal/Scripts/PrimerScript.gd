be7f0cde6022caf731600f1a61e81c4fb4a14152
Cembo14ES <inigocemborain9@gmail.com>
2026-07-16 15:58:33 +0200

    Personaje disparando

--- video-juego-grupal/Scripts/PrimerScript.gd
+++ video-juego-grupal/Scripts/PrimerScript.gd
@@ -3,12 +3,14 @@ extends CharacterBody3D
 # Velocidad de movimiento
 const SPEED = 5.0
 
+# 1. Cargamos el objeto que queremos que aparezca (arrastra tu archivo .tscn aquí)
+const Bullet = preload("res://Prefabs/bullet.tscn")
+
 func _physics_process(delta):
 	# 1. Leer las teclas pulsadas (AWSD)
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	
 	# 2. Convertir los controles a una dirección en 3D
-	# Ponemos un '0' en el medio porque no queremos que se mueva en el eje Y (arriba/abajo)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	# 3. Aplicar la velocidad
@@ -22,3 +24,18 @@ func _physics_process(delta):
 
 	# 4. Ejecutar el movimiento
 	move_and_slide()
+	
+	# 5. Detectar el ataque e instanciar el objeto
+	if Input.is_action_just_pressed("atack"):
+		atacar()
+
+func atacar():
+	# Creamos una copia (instancia) del objeto
+	var nuevo_objeto = Bullet.instantiate()
+	
+	# Lo añadimos a la escena principal (en el "root" o padre para que no se mueva con el jugador)
+	get_parent().add_child(nuevo_objeto)
+	var offset_local = Vector3(2.0, 0.0, 0.2)
+	
+	# Le damos la posición actual del jugador para que aparezca donde estamos
+	nuevo_objeto.global_position = global_position + (global_transform.basis * offset_local)
--- video-juego-grupal/Scripts/SegundoScript.gd
+++ video-juego-grupal/Scripts/SegundoScript.gd
@@ -1,11 +1,21 @@
-extends Node
+extends Node3D # O cambia "Node3D" por el tipo de nodo que sea tu objeto (ej. Area3D)
 
+# Velocidad a la que avanza
+const VELOCIDAD = 6.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Tiempo de vida del objeto en segundos
+const TIEMPO_VIDA = 3.0
 
+func _ready():
+	# Creamos un temporizador (Timer) por código para que destruya el objeto a los 3 segundos
+	var timer = get_tree().create_timer(TIEMPO_VIDA)
+	
+	# Cuando el temporizador termine, llamamos a "queue_free()" para borrar el objeto del juego
+	timer.timeout.connect(queue_free)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	# Mueve el objeto hacia adelante (eje Z negativo en Godot) constantemente.
+	# "global_transform.basis.z" es la dirección hacia donde "mira" el objeto en el espacio 3D.
+	# En Godot, el "frente" de un objeto suele ser hacia el Z negativo (-z).
+	
+	global_position += -global_transform.basis.z * VELOCIDAD * delta
